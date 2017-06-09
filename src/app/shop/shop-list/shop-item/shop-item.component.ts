58f03918366eb9bbbefd34990543624a28294510
AlekseySamoylov <alekseysamoylov89@gmail.com>
2017-06-09 22:15:55 +0300

    Little changes.

--- src/app/shop/shop-list/shop-item/shop-item.component.ts
+++ src/app/shop/shop-list/shop-item/shop-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Material} from '../../../shared/material.model';
 
 @Component({
   selector: 'app-shop-item',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopItemComponent implements OnInit {
 
+  @Input('shopItem') material: Material;
   constructor() { }
 
   ngOnInit() {
