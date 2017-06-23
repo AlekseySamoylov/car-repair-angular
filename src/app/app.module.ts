1ec10070c9c659a2ad9c33f9d1713a0d870a9169
AlekseySamoylov <alekseysamoylov89@gmail.com>
2017-06-23 23:24:56 +0300

    Routing added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import {HttpModule, JsonpModule} from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule, JsonpModule} from "@angular/http";
+import {RouterModule, Routes} from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import {HeaderComponent} from './header/header.component';
-import {PricesComponent} from './prices/prices.component';
-import { ShopComponent } from './shop/shop.component';
-import { ShopListComponent } from './shop/shop-list/shop-list.component';
-import { ShopDetailComponent } from './shop/shop-detail/shop-detail.component';
-import { ShopItemComponent } from './shop/shop-list/shop-item/shop-item.component';
-import { BasketComponent } from './basket/basket.component';
-import { BasketListComponent } from './basket/basket-list/basket-list.component';
-import { BasketEditComponent } from './basket/basket-list/basket-edit/basket-edit.component';
-import { PriceListComponent } from './prices/price-list/price-list.component';
-import { PriceItemComponent } from './prices/price-list/price-item/price-item.component';
-import { PriceManagerComponent } from './prices/price-list/price-manager/price-manager.component';
-import { AboutComponent } from './about/about.component';
-import { CarRepairComponent } from './car-repair/car-repair.component';
-import {BasicHighlightDirective} from './custom-directives/basic-highlight.directive';
-import { BetterHighlightDirective } from './custom-directives/better-highlight.directive';
-import { UnlessDirective } from './custom-directives/unless.directive';
-import {DropdownDirective} from './custom-directives/dropdown.directive';
-import { AccountComponent } from './admin-panel/account/account.component';
-import { NewAccountComponent } from './admin-panel/new-account/new-account.component';
-import {AdminPanelComponent} from './admin-panel/admin-panel.component';
-import {LoggingService} from './utils/logging.service';
+import {AppComponent} from "./app.component";
+import {ContactsComponent} from "./contacts/contacts.component";
+import {HeaderComponent} from "./header/header.component";
+import {PricesComponent} from "./prices/prices.component";
+import {ShopComponent} from "./shop/shop.component";
+import {ShopListComponent} from "./shop/shop-list/shop-list.component";
+import {ShopDetailComponent} from "./shop/shop-detail/shop-detail.component";
+import {ShopItemComponent} from "./shop/shop-list/shop-item/shop-item.component";
+import {BasketComponent} from "./basket/basket.component";
+import {BasketListComponent} from "./basket/basket-list/basket-list.component";
+import {BasketEditComponent} from "./basket/basket-list/basket-edit/basket-edit.component";
+import {PriceListComponent} from "./prices/price-list/price-list.component";
+import {PriceItemComponent} from "./prices/price-list/price-item/price-item.component";
+import {PriceManagerComponent} from "./prices/price-list/price-manager/price-manager.component";
+import {AboutComponent} from "./about/about.component";
+import {CarRepairComponent} from "./car-repair/car-repair.component";
+import {BasicHighlightDirective} from "./custom-directives/basic-highlight.directive";
+import {BetterHighlightDirective} from "./custom-directives/better-highlight.directive";
+import {UnlessDirective} from "./custom-directives/unless.directive";
+import {DropdownDirective} from "./custom-directives/dropdown.directive";
+import {AccountComponent} from "./admin-panel/account/account.component";
+import {NewAccountComponent} from "./admin-panel/new-account/new-account.component";
+import {AdminPanelComponent} from "./admin-panel/admin-panel.component";
+import {LoggingService} from "./utils/logging.service";
+
+const appRoutes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'prices', component: PricesComponent},
+  {path: 'car-repair', component: CarRepairComponent},
+  {path: 'shop', component: ShopComponent},
+  {path: 'basket', component: BasketComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'admin-panel', component: AdminPanelComponent}
+];
 
 @NgModule({
   declarations: [
@@ -58,7 +69,8 @@ import {LoggingService} from './utils/logging.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [LoggingService],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {HeaderTab} from '../shared/header-tab.enum';
+import {Component, EventEmitter, Output} from "@angular/core";
+import {HeaderTab} from "../shared/header-tab.enum";
 
 const MAGIC_NUMBER = 768;
 
@@ -9,39 +9,11 @@ const MAGIC_NUMBER = 768;
   styleUrls: ['header.component.css']
 })
 export class HeaderComponent {
-  tabNumber = 1;
   @Output() menuTabSelected = new EventEmitter<HeaderTab>();
 
-  headerSelect(tabNumber: number) {
-    this.tabNumber = tabNumber;
+  headerSelect() {
     if (screen.width < MAGIC_NUMBER) {
       document.getElementById('nav-bar-button').click();
     }
   }
-  isHeaderTabSelected(tabNumber: number) {
-    return this.tabNumber === tabNumber;
-  }
-
-  openMainMenu() {
-    this.menuTabSelected.emit(HeaderTab.MAIN);
-  }
-  openPriceMenu() {
-    this.menuTabSelected.emit(HeaderTab.PRICES);
-  }
-  openCarRepairMenu() {
-    this.menuTabSelected.emit(HeaderTab.CAR_REPAIR);
-  }
-  openShopMenu() {
-    this.menuTabSelected.emit(HeaderTab.SHOP);
-  }
-  openContactsMenu() {
-    this.menuTabSelected.emit(HeaderTab.CONTACTS);
-  }
-  openBasketMenu() {
-    this.menuTabSelected.emit(HeaderTab.BASKET);
-  }
-  openAdminMenu() {
-    this.menuTabSelected.emit(HeaderTab.ADMIN_PANEL);
-  }
-
 }
